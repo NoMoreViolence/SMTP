19c36b58a7c2083b116b2a5b9b8e6fd1615d436b
NoMoreViolence <ljh86029926@gmail.com>
2020-05-31 21:38:19 +0900

    chore: export to import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
-export * from './spam-mail-filter';
-export * from './slack';
-export * from './constants';
+import './spam-mail-filter';
+import './slack';
