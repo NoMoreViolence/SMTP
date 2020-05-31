02bc5229858f3c4d6863a7b5ed35860b2a043bcb
NoMoreViolence <ljh86029926@gmail.com>
2020-06-01 02:32:07 +0900

    chore: add test log

--- src/slack/slack-noti.ts
+++ src/slack/slack-noti.ts
@@ -4,34 +4,36 @@ import aws from 'aws-sdk';
 import parse from 'emailjs-mime-parser';
 
 const bucketName = process.env.S3_BUCKET || 'wrong_bucket';
-const s3 = new aws.S3({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
-});
-
 const channel = process.env.SLACK_CHANNEL || '#random';
 const webhookUrl = process.env.SLACK_END_POINT || 'https://whitehouse.com';
-const webhook = new IncomingWebhook(webhookUrl, {
-  channel: channel,
-});
 
 export async function slackNoti(event: any, context: Context, callback: Callback) {
+  const s3 = new aws.S3({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  });
+  const webhook = new IncomingWebhook(webhookUrl, {
+    channel: channel,
+  });
+
   const eventObject = JSON.stringify(event.Records[0].ses.mail);
   const { messageId, timestamp, source } = event.Records[0].ses.mail;
+
+  await webhook.send('메일 송 수신 테스트 시작');
 
   s3.getObject(
     {
       Bucket: bucketName,
       Key: messageId,
     },
-    function (err, data) {
+    async function (err, data) {
       if (err) {
         callback(null, null);
       } else {
         const emailMimeNode = parse(data.Body);
-        webhook.send('테스트 메일임');
-        webhook.send(eventObject);
-        webhook.send(emailMimeNode);
+        await webhook.send('테스트 메일임');
+        await webhook.send(eventObject);
+        await webhook.send(emailMimeNode);
         callback(null, null);
       }
     }
