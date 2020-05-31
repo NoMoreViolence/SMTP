5b381cb7bfb37039dd6e93cfe378487dd00e0039
NoMoreViolence <ljh86029926@gmail.com>
2020-06-01 02:57:50 +0900

    chore: remove async

--- src/slack/slack-noti.ts
+++ src/slack/slack-noti.ts
@@ -7,7 +7,7 @@ const bucketName = process.env.S3_BUCKET || 'wrong_bucket';
 const channel = process.env.SLACK_CHANNEL || '#random';
 const webhookUrl = process.env.SLACK_END_POINT || 'https://whitehouse.com';
 
-export async function slackNoti(event: any, context: Context, callback: Callback) {
+export function slackNoti(event: any, context: Context, callback: Callback) {
   const s3 = new aws.S3({
     accessKeyId: process.env.ACCESS_KEY_ID,
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
@@ -19,17 +19,17 @@ export async function slackNoti(event: any, context: Context, callback: Callback
   const eventObject = JSON.stringify(event.Records[0].ses.mail);
   const { messageId, timestamp, source } = event.Records[0].ses.mail;
 
-  await webhook.send('메일 송 수신 테스트 시작');
-  await webhook.send(messageId);
+  webhook.send('메일 송 수신 테스트 시작');
+  webhook.send(messageId);
 
   s3.getObject(
     {
       Bucket: bucketName,
       Key: messageId,
     },
-    async function (err, data) {
+    function (err, data) {
       if (err) {
-        await webhook.send(`
+        webhook.send(`
           S3에서 데이터를 받아오지 못했습니다.
           로그 정보:
             bucketName: ${bucketName}
@@ -37,17 +37,17 @@ export async function slackNoti(event: any, context: Context, callback: Callback
         `);
         callback(err);
       } else {
-        await webhook.send(`S3에서 데이터를 불러오는 데 성공했습니다.`);
-        await webhook.send(data.Body?.toString() ?? '');
+        webhook.send(`S3에서 데이터를 불러오는 데 성공했습니다.`);
+        webhook.send(data.Body?.toString() ?? '');
 
         try {
           const emailMimeNode = parse(data.Body);
-          await webhook.send(`모든 작업 성공`);
-          await webhook.send(eventObject);
-          await webhook.send(emailMimeNode);
+          webhook.send(`모든 작업 성공`);
+          webhook.send(eventObject);
+          webhook.send(emailMimeNode);
           callback(null, null);
         } catch (e) {
-          await webhook.send(`mime parse failed`);
+          webhook.send(`mime parse failed`);
           callback(null, null);
         }
       }
