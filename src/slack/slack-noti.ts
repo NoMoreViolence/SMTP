b835d38601bd7547527991e0ce88e00e49b7ad9f
NoMoreViolence <ljh86029926@gmail.com>
2020-06-02 00:01:18 +0900

    feat: createWriteStream with fs

--- src/slack/slack-noti.ts
+++ src/slack/slack-noti.ts
@@ -3,6 +3,7 @@ import { IncomingWebhook } from '@slack/client';
 import { Callback, Context } from 'aws-lambda';
 import aws from 'aws-sdk';
 import dayjs from 'dayjs';
+import * as fs from 'fs';
 
 const signingSecret = process.env.SIGNING_SECRET || 'mkit';
 const bucket = process.env.S3_BUCKET_ID || 'justhis';
@@ -27,28 +28,35 @@ export function slackNoti(event: any, _context: Context, callback: Callback) {
     });
     const { messageId, timestamp } = event.Records[0].ses.mail;
 
+    const file = fs.createWriteStream(`./${timestamp}.eml`);
     const readStream = s3
       .getObject(
         {
           Bucket: bucket,
           Key: messageId,
         },
-        function (err, data) {
+        async function (err, data) {
           if (err) {
             webhook.send('메일을 받았으나, 에러로 인해서 가져오지 못했습니다. :)');
             throw new Error('Bad request');
           }
         }
       )
-      .createReadStream();
+      .createReadStream()
+      .pipe(file);
 
-    slackApp.client.files.upload({ channels: channel, token: slackBotToken, file: readStream });
-    webhook.send(
-      `[새로운 이메일이 도착했습니다.] ${dayjs(new Date(timestamp).toISOString()).format(
-        'YYYY.MM.DD hh.mm A (+9)'
-      )}\n첨부파일을 다운받으 신 후, 메일 확장자를 .eml로 변경하게 되면 메일을 읽을 수 있게 됩니다.`
-    );
-    callback(null, null);
+    slackApp.client.files
+      .upload({ channels: channel, token: slackBotToken, file: readStream })
+      .then(() => {
+        webhook.send(
+          `[새로운 이메일이 도착했습니다.] ${dayjs(new Date(timestamp).toISOString()).format(
+            'YYYY.MM.DD hh.mm A (+9)'
+          )}\n첨부파일을 다운받으 신 후, 메일 확장자를 .eml로 변경하게 되면 메일을 읽을 수 있게 됩니다.`
+        );
+      })
+      .finally(() => {
+        callback(null, null);
+      });
   } catch (e) {
     callback(null, null);
   }
